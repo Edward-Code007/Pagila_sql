106d46e8bf36cdef7e39a7ccc0e753f32f0c20bc
Edward-Code007 <glezmoradev@gmail.com>
2026-07-12 08:47:18 -0400

    optimquery

--- RespuestaEjerciciosCLAUDE/preguntas#5.sql
+++ RespuestaEjerciciosCLAUDE/preguntas#5.sql
@@ -11,30 +11,53 @@
 -- 43. Ejecuta EXPLAIN sobre una consulta que filtre customer por last_name = 'SMITH'.
 --     Identifica en el plan: ¿qué tipo de scan usa? ¿tiene índice o hace Seq Scan?
 --     Anota el costo estimado y el número estimado de filas.
-
+explain
+select * from customer c where c.last_name = 'SMITH';
 
 -- 44. Compara EXPLAIN vs EXPLAIN ANALYZE sobre la misma consulta del ejercicio 43.
 --     Observa la diferencia entre las filas estimadas y las filas reales,
 --     y anota el tiempo real de ejecución (Execution Time).
-
+explain analyze
+select * from customer c where c.last_name = 'SMITH';
 
 -- 45. Ejecuta EXPLAIN ANALYZE sobre una consulta que haga JOIN de customer,
 --     rental y payment filtrando por un customer_id específico. Identifica
 --     qué tipos de JOIN eligió el planner (Hash Join, Nested Loop, Merge Join)
 --     y por qué crees que eligió cada uno.
-
+explain analyze
+select * from customer 
+join rental using(customer_id)
+join payment using(rental_id);
 
 -- ----- CREACIÓN DE ÍNDICES B-TREE -----
 
 -- 46. Crea un índice B-tree simple sobre customer.last_name. Antes y después,
 --     ejecuta EXPLAIN ANALYZE sobre una búsqueda por apellido y compara
 --     el tipo de scan (Seq Scan → Index Scan) y el tiempo de ejecución.
-
+create index i_last_name_customer 
+on customer using btree(last_name); 
 
 -- 47. Crea un índice compuesto sobre (customer.store_id, customer.active).
 --     Prueba tres consultas: (a) filtrando por ambas columnas, (b) solo
 --     store_id, (c) solo active. ¿En cuáles se usa el índice? Explica por qué
 --     (pista: el orden de las columnas importa — regla del "prefijo izquierdo").
+create index if not exists ix_customer_store_id_active
+on customer(store_id,active);
+
+set enable_seqscan = OFF;
+set enable_seqscan = ON;
+
+explain analyze
+select * from customer
+where store_id = 1 and active = 1;
+
+explain analyze
+select * from customer
+where store_id = 1;
+
+explain analyze
+select * from customer
+where  active = 1;
 
 
 -- 48. Crea un índice parcial (partial index) que solo incluya customer.email
@@ -42,7 +65,15 @@
 --     parcial vs un índice completo sobre email con la consulta:
 --     SELECT pg_size_pretty(pg_relation_size('nombre_del_indice'));
 
+create index partial_index
+on customer(email)
+where active = 1;
 
+create index full_index
+on customer(email);
+
+select pg_size_pretty(pg_relation_size('full_index'));
+select pg_size_pretty(pg_relation_size('partial_index'));
 -- ----- SARGABILITY (funciones sobre columnas indexadas) -----
 
 -- 49. Con el índice de last_name creado en el 46, prueba EXPLAIN sobre:
@@ -52,11 +83,20 @@
 --     (d) WHERE last_name LIKE '%MITH'
 --     Identifica cuáles usan el índice y cuáles NO (y por qué).
 
+explain
+select * from customer c where c.last_name = 'SMITH'; --Sarg-Ability
+explain
+select * from customer c where lower(c.last_name) = 'smith'--no transforma un campo
+explain
+select * from customer c where c.last_name like 'SMI%' --Puede usar indice en un caso en especifico(Leer mas)
+explain
+select * from customer c where c.last_name like '%ITH';--Comodin al inicio de la expresion no es Sarg-able
 
 -- 50. Crea un índice funcional (expression index) sobre LOWER(last_name)
 --     para que la búsqueda case-insensitive del ejercicio 49-b sí use índice.
 --     Verifica con EXPLAIN que ahora sí lo usa.
-
+create index expression_index
+on customer(lower(last_name));
 
 -- ----- ÍNDICES GIN PARA BÚSQUEDA AVANZADA -----
 
@@ -76,19 +116,63 @@
 --     ejecuta EXPLAIN ANALYZE. Identifica el nodo más costoso. ¿Podrías
 --     agregar algún índice que ayude? Prueba tu hipótesis y mide la mejora.
 
+explain analyze
+WITH film_cont_by_cat AS (
+    SELECT c."name", f.title, c.category_id, COUNT(f.film_id) AS "Cont_Film"
+    FROM film f
+    JOIN film_category fc USING (film_id)
+    JOIN category c USING (category_id)
+    JOIN inventory i USING (film_id)
+    JOIN rental r USING (inventory_id)
+    GROUP BY c.category_id, f.film_id
+),
+set_ranking AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY fc.category_id ORDER BY "Cont_Film" DESC) AS ranking
+    FROM film_cont_by_cat fc
+)
+SELECT "name", title, "Cont_Film" FROM set_ranking
+WHERE "ranking" = 1;
+--Nodo mas costos SEQ SCAN RENTAL
+--Como es SEQ scan es necesario buscar la tabla completa 
+--para hacer el join por lo q un indice no ayuda aqui
 
 -- 54. Escribe DOS versiones de la misma consulta: "clientes que han rentado
 --     películas de la categoría 'Horror'":
 --     Versión A: usando JOIN a category
 --     Versión B: usando WHERE customer_id IN (subconsulta con Horror)
---     Compara los planes de EXPLAIN ANALYZE. ¿Cuál es más eficiente y por qué?
+--     Compara los planes de EXPLAIN ANALYZE. ¿Cuál es más eficiente y por qué
+explain analyze
+select distinct customer_id ,first_name from customer
+join rental using(customer_id)
+join inventory using(inventory_id)
+join film using(film_id)
+join film_category using(film_id)
+join category c using(category_id)
+where c."name" = 'Horror';
 
+explain analyze
+select first_name from customer
+where customer_id in (
+	select customer_id from rental
+	join inventory using(inventory_id)
+	join film using(film_id)
+		where film_id in ( 
+		select film_id from film_category --Horror Films
+		join category using(category_id)
+		where "name" = 'Horror'
+		)
+) 
 
 -- 55. Considera esta consulta con función sobre columna:
 --     WHERE EXTRACT(YEAR FROM payment_date) = 2022
 --     Reescríbela de forma SARGable (usando rangos de fecha) y compara los
 --     planes. ¿Cuál permite usar índices? (pista: BETWEEN o >= AND <)
-
+explain analyze --Planning 0.488 Execution: 3.169
+select * from payment p
+where p.payment_date between '2022-01-1' and '2022-12-31';
+explain analyze --Planning 0.177 Execution: 5.067
+select * from payment p
+where extract(year from payment_date) = 2022;
 
 -- ----- MANTENIMIENTO Y ESTADÍSTICAS -----
 
@@ -96,7 +180,7 @@
 --     pg_stats para ver las estadísticas que el planner usa:
 --     SELECT * FROM pg_stats WHERE tablename = 'customer';
 --     Explica qué campos como n_distinct y most_common_vals significan.
-
+	select * from pg_stats
 
 -- 57. Lista todos los índices existentes en la tabla film con:
 --     SELECT * FROM pg_indexes WHERE tablename = 'film';
--- RespuestaEjerciciosCLAUDE/preguntas#7.sql
+++ RespuestaEjerciciosCLAUDE/preguntas#7.sql
@@ -99,12 +99,43 @@ select * from estadisticas_categoria('Horror');
 --     categoría. Usa un LOOP con FOR ... IN SELECT ... para iterar sobre las
 --     filas. Pista: la sintaxis es:
 --     FOR fila IN SELECT ... FROM ... LOOP ... END LOOP;
-
-
+create or replace function contar_palabras_titulos(nombre_cat text)
+returns int
+language plpgsql as
+$$
+declare
+cantidad_palabras int := 0;
+fila record;
+begin
+for fila in select title from 
+ (select f.title from category c
+join film_category fc using(category_id)
+join film f using(film_id)
+where c."name" = nombre_cat) loop
+ cantidad_palabras := cantidad_palabras + array_length(string_to_array(fila.title, ' '), 1);
+end loop;
+return cantidad_palabras;
+end;
+$$
+select contar_palabras_titulos('Horror');
 -- 63. Crea un procedimiento `resetear_precios(nombre_cat text)` que recorra
 --     las películas de una categoría e imprima con RAISE NOTICE cada una junto
 --     con su rental_rate antes de resetearlo a 2.99. Usa el mismo patrón FOR-LOOP.
-
+create or replace procedure resetear_precios(nombr_cat text)
+language plpgsql as
+$$
+declare
+fila record;
+begin
+for fila in (select film_id,rental_rate,title from film f
+join film_category fc using(film_id)
+join category c using(category_id)
+where c."name" = nombr_cat) loop
+	raise notice 'Estado Filme antes del cambio: Title: % , Rental_Rate: %', fila.title,fila.rental_rate;
+	update film set rental_rate = 2.99 where film_id = fila.film_id;
+end loop;
+end;
+$$
 -- ----- JSON / JSONB (TEMA NUEVO) -----
 
 -- 64. Verifica el tipo de la columna 'settings' en la tabla store — probablemente
@@ -112,7 +143,18 @@ select * from estadisticas_categoria('Horror');
 --     (customer_id int PRIMARY KEY, datos jsonb).
 --     Inserta 3 filas con estructuras JSON como:
 --     '{"telefono": "555-1234", "preferencias": {"idioma": "es", "notificar": true}, "tags": ["premium", "activo"]}'
-
+select * from store;
+create temp table cliente_metadata(customer_id int primary key,datos jsonb);
+insert into cliente_metadata values(2,
+'{"idioma": "es", 
+"notificar": true, 
+"tags": ["premium", "activo"],
+"settings": {
+	"dark_mode": false,
+	"accept_cookies": true
+}
+}');
+select * from cliente_metadata;
 
 -- 65. Sobre la tabla `cliente_metadata` del ejercicio 64, prueba los operadores
 --     básicos de JSONB:
@@ -121,26 +163,67 @@ select * from estadisticas_categoria('Horror');
 --     - `datos -> 'preferencias' -> 'idioma'` (navegación anidada)
 --     - `datos -> 'tags' -> 0`     (índice de array)
 --     Anota la diferencia entre `->` y `->>`.
-
+select datos -> 'idioma' from cliente_metadata where customer_id = 2;
+select datos -> 'settings' from cliente_metadata  where customer_id = 2; --Modo JSONB
+select datos ->> 'settings' from cliente_metadata  where customer_id = 2; --Modo Text
+select datos #> '{settings,dark_mode}' from  cliente_metadata  where customer_id = 2; --Modo JSONB
+select datos -> 'tags' ->> 0 from  cliente_metadata  where customer_id = 2; --Array POS
+select * from cliente_metadata where datos ? 'settings';
 
 -- 66. Consulta la tabla `cliente_metadata` filtrando por contenido JSON.
 --     Encuentra los clientes cuyo JSON contiene `{"preferencias": {"idioma": "es"}}`
 --     usando el operador `@>` (contiene). Anota también el uso del operador `?`
 --     para verificar si existe una clave: `datos ? 'telefono'`.
+select * from cliente_metadata where datos @> '{"settings":{"dark_mode" : false}}';
 
+select * from cliente_metadata where datos ? 'tags';
+select * from cliente_metadata where datos ?| array['tags','notificar'];
+select * from cliente_metadata where datos ?& array['tags','notificar'];
 
 -- 67. Crea una función `agregar_tag_cliente(id_cliente int, nuevo_tag text)`
 --     que modifique el array `tags` dentro del JSON de un cliente, agregando
 --     un nuevo tag al final. Usa `jsonb_set` y el operador de concatenación `||`.
 --     Prueba: SELECT agregar_tag_cliente(1, 'nuevo_tag');
-
+create or replace function agregar_tag_cliente(id_cliente int,nuevo_tag text)
+returns void
+language plpgsql as 
+$$
+declare
+begin
+if not exists(select 1 from cliente_metadata cm 
+where cm.customer_id = id_cliente) then
+	raise exception 'Cliente con id % no existe' , id_cliente;
+else 
+	update cliente_metadata set datos = jsonb_set(
+	datos,
+	'{tags}',
+	coalesce((datos -> 'tags'),'[]'::jsonb) || to_jsonb(ARRAY[nuevo_tag]))
+	where customer_id = id_cliente;
+end if;
+raise notice 'Operacion completada con Exito';
+end;
+$$
+select agregar_tag_cliente(2,'ELTANKE');
 
 -- 68. Crea una función `stats_cliente_json(id_cliente int)` que devuelva las
 --     estadísticas del cliente COMO JSON: 
 --     {"customer_id": 1, "nombre": "MARY", "total_rentas": 33, "total_gastado": 118.68}
 --     Usa la función `jsonb_build_object('key', value, ...)`.
+create or replace function stats_cliente_json(id_cliente int)
+returns json
+language plpgsql as
+$$
+declare
+json_to_return json;
+begin
 
-
+select jsonb_build_object('nombre',first_name,'customer_id', customer_id) into json_to_return 
+from customer c 
+where c.customer_id = id_cliente;
+return json_to_return;
+end;
+$$
+select stats_cliente_json(2);
 -- ----- TRIGGERS AVANZADOS -----
 
 -- 69. Crea una tabla `auditoria_precios` con:
@@ -150,18 +233,67 @@ select * from estadisticas_categoria('Horror');
 --      "despues": {"rental_rate": 3.29, "rental_duration": 6}}
 --     Crea un trigger AFTER UPDATE ON film que registre estos cambios usando
 --     `jsonb_build_object`. Combina PL/pgSQL + JSON.
+create temp table auditoria_precios(id serial Primary Key, film_id int,cambio jsonb, fecha timestamp default now());
+drop table auditoria_precios;
+create or replace function f_registrar_cambios()
+returns trigger
+language plpgsql as
+$$
+declare
+base_json jsonb;
+begin
+base_json := jsonb_build_object('before',to_jsonb(old),'after',to_jsonb(new));
+insert into auditoria_precios(film_id,cambio)
+values(new.film_id,base_json);
+return new;
+end;
+$$
 
+create or replace trigger tr_auditoria_precios
+after update of rental_rate,rental_duration on film
+for each row
+execute function f_registrar_cambios();
 
+update film set title = title || ' ' where film_id = 1;
+select * from auditoria_precios;
+drop trigger tr_audit_films_updates on film;
 -- 70. Modifica el trigger del ejercicio 69 para que use `WHEN (OLD.rental_rate 
 --     IS DISTINCT FROM NEW.rental_rate)` — que solo se dispare cuando el precio
 --     realmente cambie, ignorando UPDATEs que tocan otras columnas.
 
+create or replace trigger tr_auditoria_precios
+after update on film 
+for each row
+when (old.rental_rate is distinct from new.rental_rate)
+execute function f_registrar_cambios();
 
 -- 71. Crea un trigger BEFORE INSERT ON rental que, si el customer_id pertenece
 --     a un cliente con 'active = 0' (inactivo), rechace la renta con
 --     RAISE EXCEPTION. Además, si el cliente tiene 10+ rentas sin devolver
 --     (return_date IS NULL), también rechazar. Combina múltiples validaciones.
+create or replace function f_check_customer()
+returns trigger
+language plpgsql as 
+$$
+declare
+id_cliente int := new.customer_id;
+begin
+if exists(select 1 from customer c where c.customer_id = id_cliente and c.active = 0) then
+	raise exception 'Usuario Inactivo'; 
+end if;
+if 10 <= (select count(*) from customer c
+join rental r using(customer_id)
+where r.return_date is null) then
+	raise exception 'Devuelva Primero las anteriores Rentadas';
+end if;
+return new;
+end;
+$$
 
+create or replace trigger tr_check_customer
+before insert on rental
+for each row
+execute function f_check_customer();
 
 -- ----- MANEJO DE ERRORES ESPECÍFICOS -----
 
@@ -172,12 +304,73 @@ select * from estadisticas_categoria('Horror');
 --     - WHEN OTHERS THEN devuelve NULL (fallback)
 --     Practica el manejo diferenciado según tipo de error.
 
+create or replace function f_insertar_actor_seguro(nombre text,apellido text)
+returns int
+language plpgsql as
+$$
+declare
+new_id int;
+begin
+if  nombre is  null or
+	apellido is  null or
+	length(trim(nombre)) = 0 or
+	length(trim(apellido)) = 0
+then
+	raise exception  'Nombre o Apellidos null o vacios';
+end if;
+	insert into actor(first_name,last_name)
+			   values(nombre,apellido) 
+			   returning actor_id into new_id;
+return new_id;
+exception 
+	when unique_violation then
+		raise notice 'Se intento insertar un valor ya existente en una columna unique: Err: %',SQLERRM;
+	when not_null_violation then
+		raise notice 'Se intento asignar null a una columna not null: Err: % ', SQLERRM;
+	when others then
+		raise notice 'Fallo Insert: %', SQLERRM;
+		-- raise; funciona como re-throw;
+		return null;
+end;
+$$
 
 -- 73. Crea un procedimiento `transferir_categoria(id_film int, nombre_nueva_cat text)`
 --     que cambie la categoría de una película. Use un bloque BEGIN...EXCEPTION
 --     que si algo falla (categoría inexistente, película inexistente), haga
 --     RAISE NOTICE con el error y NO propague la excepción al caller.
+create or replace procedure p_transferir_categoria(id_film int,nombre_nueva_cat text)
+language plpgsql as
+$$
+declare
+id_category int;
+begin
+	select c.category_id into id_category from category c where c."name" = nombre_nueva_cat;
+	if not exists(select 1 from film where film_id = id_film) 
+	then
+		raise exception 'Pelicula no Existe' using errcode = 'CUH01';
+	end if;
+	if id_category is null 
+	then
+		raise exception 'Categoria no Existe' using errcode = 'CUH02';
+	end if;
+	update film_category fc 
+	set category_id = id_category 
+	where film_id = id_film;
 
+	exception
+		when SQLSTATE 'CUH01' then
+			raise notice 'error: %', SQLERRM;
+		when SQLSTATE 'CUH02' then
+			raise notice 'error: %', SQLERRM; 
+end;
+$$
+begin;
+call p_transferir_categoria(1,'Diaz Canel Singao');
+select film_id,"name" from film
+join film_category using(film_id)
+join category using(category_id)
+order by film_id;
+rollback;
 -- ----- CONSULTA Y LIMPIEZA -----
 
 -- 74. Consulta el pg_catalog para listar tus triggers activos:
@@ -186,7 +379,7 @@ select * from estadisticas_categoria('Horror');
 --     WHERE trigger_schema = 'public'
 --     ORDER BY event_object_table;
 --     Identifica cuáles son tuyos y cuáles son originales de Pagila.
-
+select * from information_schema.triggers;
 
 -- 75. Limpia todos los objetos creados en esta tanda con DROP correspondientes.
 --     Verifica con \df y consultando information_schema.triggers.
